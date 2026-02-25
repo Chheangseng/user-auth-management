87a4e99f54e6ceaa9061590cc7e58589c0a58007
CHHEANGSENGTAIN\Admin <taingchheangseng123@gmail.com>
2026-02-25 14:28:08 +0700

    Fix: remove unneed config

--- src/main/java/com/tcs/user_auth_management/UserAuthManagementApplication.java
+++ src/main/java/com/tcs/user_auth_management/UserAuthManagementApplication.java
@@ -1,6 +1,5 @@
 package com.tcs.user_auth_management;
 
-import com.tcs.user_auth_management.config.admin.SystemAdminProperty;
 import com.tcs.user_auth_management.config.mailSender.MailConfigProperties;
 import com.tcs.user_auth_management.config.security.jwt.RSAProperties;
 import org.springframework.boot.SpringApplication;
@@ -9,16 +8,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		SystemAdminProperty.class,
-		RSAProperties.class,
-		MailConfigProperties.class
-})
+@EnableConfigurationProperties({RSAProperties.class, MailConfigProperties.class})
 @EnableScheduling
 public class UserAuthManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserAuthManagementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserAuthManagementApplication.class, args);
+  }
 }
