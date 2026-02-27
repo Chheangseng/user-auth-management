8f96658f9cbb780ec11fc8ee9d6ec669efd35506
CHHEANGSENGTAIN\Admin <taingchheangseng123@gmail.com>
2026-02-27 16:57:03 +0700

    Fix: add controller advice for catch Exception

--- src/main/java/com/tcs/user_auth_management/exception/ApiExceptionHandler.java
+++ src/main/java/com/tcs/user_auth_management/exception/ApiExceptionHandler.java
@@ -3,6 +3,8 @@ package com.tcs.user_auth_management.exception;
 import com.tcs.user_auth_management.exception.dto.ApiException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ApiExceptionHandler {
   @ExceptionHandler(value = {ApiExceptionStatusException.class})
   public ResponseEntity<Object> handleTechnicalException(ApiExceptionStatusException e) {
@@ -30,6 +33,19 @@ public class ApiExceptionHandler {
             400,
             httpStatus,
             ZonedDateTime.now(ZoneId.of("Z")));
+    return new ResponseEntity<>(apiException, httpStatus);
+  }
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> handleAllUncaughtExceptions(Exception ex) {
+    // This ensures that ANY unexpected 500 error always shows a trace
+    log.error("CRITICAL UNEXPECTED ERROR: ", ex);
+    HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    var apiException =
+            new ApiException(
+                   "An internal server error occurred",
+                    httpStatus.value(),
+                    httpStatus,
+                    ZonedDateTime.now(ZoneId.of("Z")));
     return new ResponseEntity<>(apiException, httpStatus);
   }
 }
